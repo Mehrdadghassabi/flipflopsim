f3e20b66ebc7a7808021c727a85b61e403b72807
mehrdadghassabi <53050138+Mehrdadghassabi@users.noreply.github.com>
2022-07-11 01:50:22 +0430

    Add files via upload

---
+++ FliflopSimulator/MainForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Dour-Andish
+ * Date: 12/20/2019
+ * Time: 2:04 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace FliflopSimulator
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private IContainer components = null;
+		private Button button1;
+		private Label label1;
+		private TextBox textBox1;
+		private Label label2;
+		private TextBox textBox2;
+		private Label label3;
+		private TextBox textBox3;
+		//adding by code
+		private TextBox textBox4;
+		private Label label4;
+		private Label label5;
+		private Label label6;
+		private TextBox textBox5;
+		private RadioButton radioButton1;
+		
+		/// <summary>
+		/// 
+		/// 
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+			
+	
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button1.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(724, 623);
+			this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(118, 48);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Run!";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("MV Boli", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(58, 39);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(62, 44);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Clk";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(128, 30);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(589, 43);
+			this.textBox1.TabIndex = 2;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("MV Boli", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(58, 149);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(68, 44);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "J";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(128, 140);
+			this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(589, 40);
+			this.textBox2.TabIndex = 4;
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("MV Boli", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(58, 258);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(68, 57);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "K";
+			this.label3.Click += new System.EventHandler(this.Label3Click);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(128, 248);
+			this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(589, 43);
+			this.textBox3.TabIndex = 6;
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.Location = new System.Drawing.Point(77, 631);
+			this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(130, 34);
+			this.radioButton1.TabIndex = 7;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "Is TFlipflop";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+			this.ClientSize = new System.Drawing.Size(856, 690);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "FliflopSimulator";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ FliflopSimulator/MainForm.cs
@@ -0,0 +1,232 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Dour-Andish
+ * Date: 12/20/2019
+ * Time: 2:04 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Text;
+
+namespace FliflopSimulator
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		private byte counter=0;
+		private string Clk;
+		private bool[] clkarr;
+		private string J;
+		private bool[] jarr;
+		private string K;
+		private bool[] karr;
+		private bool[] Right;
+		private bool[] Qarr;
+		private string Q;
+		private string Qperim;
+		private StringBuilder state;
+		
+		public MainForm()
+		{
+			this.state=new StringBuilder();
+			
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			if(counter==1){
+				this.Clk=this.textBox1.Text;
+				this.J=this.textBox2.Text;
+				this.K=this.textBox3.Text;
+				
+				SettingClkarr(this.Clk);
+				SettingJarr(this.J);
+				Settingkarr(this.K);
+				SettingQarr();
+				SetQ();
+				SetQperim();
+				addcomp();	
+			}
+			if(radioButton1.Checked){
+				this.textBox3.Text=this.textBox2.Text;
+				this.label2.Text="T";
+				this.label3.Text="T";
+			}
+				
+			addcomp1();
+			
+			
+			counter++;
+		}
+	
+		private void SettingClkarr(string str){
+			this.clkarr =new bool[str.Length];
+			this.Right =new bool[str.Length];
+			
+			for(int i=0;i<str.Length;i++){
+				this.clkarr[i]=(str.ToCharArray()[i]=='-');
+			}
+			
+			this.Right[0]=false;
+			for(int j=1;j<str.Length;j++){
+				this.Right[j]=(this.clkarr[j])&&(!this.clkarr[j-1]);
+				
+			}
+		}
+		
+	    private void Settingkarr(string str){
+			this.karr =new bool[str.Length];
+			
+			for(int i=0;i<str.Length;i++){
+				this.karr[i]=(str.ToCharArray()[i]=='-');
+			}
+	}
+		
+		private void SettingJarr(string str){
+			    this.jarr =new bool[str.Length];
+			
+			   for(int i=0;i<str.Length;i++){
+				this.jarr[i]=(str.ToCharArray()[i]=='-');
+			  }
+	}
+		
+		private void SettingQarr(){
+			int minlen=Minimummlen(this.K,this.J,this.Clk);
+			this.Qarr=new bool[this.K.Length];
+			this.Qarr[0]=false;
+		 for(int i=1;i<minlen;i++){
+				if(this.Right[i]){
+				
+					if(!this.jarr[i]&&!this.karr[i]){
+					this.Qarr[i]=this.Qarr[i-1];
+					}
+					
+					if(this.jarr[i]&&!this.karr[i]){
+						this.Qarr[i]=true;
+						this.state.Append("Set ");
+					}
+					
+					if(!this.jarr[i]&&this.karr[i]){
+						this.Qarr[i]=false;
+						this.state.Append("Reset ");
+					}
+					
+					if(this.jarr[i]&&this.karr[i]){
+						this.Qarr[i]=!this.Qarr[i-1];
+						this.state.Append("Toggle ");
+					}
+					
+				}
+				else{
+					this.Qarr[i]=this.Qarr[i-1];
+				}
+			  }
+		}
+		
+		private int Minimummlen(string x,string y,string z){
+			int min=(x.Length<y.Length)? x.Length:y.Length;
+			int min1=(min<z.Length)? min:z.Length;
+			
+			return min1;
+		}
+		
+		private void SetQ(){
+		int minlen=Minimummlen(this.K,this.J,this.Clk);
+		var sb=new StringBuilder();
+		for(int i=0;i<minlen;i++){
+			if(this.Qarr[i])
+				sb.Append('-');
+			else{
+				sb.Append('_');
+			}
+		}
+		
+		this.Q=sb.ToString();
+}
+		
+		private void SetQperim(){
+			var re=new StringBuilder();
+			
+			foreach(char c in this.Q){
+			if(c=='-')
+				re.Append('_');
+			else
+				re.Append('-');
+			}
+			
+			this.Qperim=re.ToString();
+		}
+		
+		private void addcomp(){
+				this.textBox4.Text=this.Q;
+				
+		     this.label5 = new Label();
+			this.label5.Font = new Font("MV Boli", 13.8F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new Point(275, 570);
+			this.label5.Name = "label5";
+			this.label5.Size = new Size(554, 559);
+			this.label5.TabIndex = 5;
+			this.label5.Text =this.state.ToString();
+			this.Controls.Add(this.label5);
+			
+			this.label6 = new Label();
+			this.label6.Font = new Font("MV Boli", 13.8F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new Point(54, 485);
+			this.label6.Name = "label6";
+			this.label6.Size = new Size(30, 153);
+			this.label6.TabIndex = 1;
+			this.label6.Text = "Q'";
+			this.Controls.Add(this.label6);
+				
+			
+		    this.textBox5 = new TextBox();
+			this.textBox5.Location = new Point(129, 475);
+			this.textBox5.Multiline = true;
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new Size(590, 42);
+			this.textBox5.TabIndex = 6;
+			this.Controls.Add(this.textBox5);
+			this.textBox5.Text=this.Qperim;
+		
+		}
+		
+		private void addcomp1(){
+				this.textBox4 = new TextBox();
+			this.textBox4.Location = new Point(129, 359);
+			this.textBox4.Multiline = true;
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new Size(590, 44);
+			this.textBox4.TabIndex = 6;
+			this.Controls.Add(this.textBox4);
+			
+			this.label4 = new Label();
+			this.label4.Font = new Font("MV Boli", 13.8F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new Point(57, 364);
+			this.label4.Name = "label4";
+			this.label4.Size = new Size(54, 39);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "Q";
+			this.Controls.Add(this.label4);
+		
+		}
+		
+		void Label3Click(object sender, EventArgs e)
+		{
+	
+		}
+	
+}
+}
